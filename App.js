a8bef3ee1e4148f6c7646717e5de6376b74e4d04
YashikaDangi <72248388+YashikaDangi@users.noreply.github.com>
2024-05-04 12:23:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const axios = require('axios');
+
+const app = express();
+
+const connectDB = async () => {
+    await mongoose.connect(`mongodb+srv://yashikadangi10:7wriJJmb2ivYUYHd@yashika.phxrsgk.mongodb.net/mydb`);
+    console.log(`DB is connected with ${mongoose.connection.host}`);
+}
+
+connectDB();
+
+const NumberSchema = new mongoose.Schema({
+    value: Number,
+});
+
+const NumberModel = mongoose.model('Number', NumberSchema);
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+async function fetchAndUpdateNumber() {
+    try {
+        const response = await axios.get('https://mempool.space/api/blocks/tip/height');
+        const number = response.data;
+        const lastSavedNumber = await NumberModel.findOne().sort({ _id: -1 });
+        if (!lastSavedNumber || number !== lastSavedNumber.value) {
+            await pushNumber(number);
+            console.log(`Number ${number} saved to the database.`);
+        } else {
+            console.log('Number is already up to date in the database.');
+        }
+    } catch (error) {
+        console.error('Error fetching and updating number:', error);
+    }
+}
+
+async function pushNumber(num) {
+    try {
+        const newNumber = new NumberModel({ value: num });
+        await newNumber.save();
+    } catch (err) {
+        console.error('Error saving number to the database', err);
+    }
+}
+
+fetchAndUpdateNumber();
+
+setInterval(fetchAndUpdateNumber, 300000);
+
+app.get('/numbers', async (req, res) => {
+    try {
+        const numbers = await NumberModel.find({}, 'value');
+        res.status(200).json({ numbers });
+    } catch (err) {
+        console.error('Error fetching numbers:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
